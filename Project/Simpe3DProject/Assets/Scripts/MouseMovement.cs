f161d3959a54cf829e7cd7082dd40ef89d656abd
Kaj0Wortel <kaj.wortel@gmail.com>
2019-09-18 13:25:45 +0200

    Added test texture and script + gitignore

--- Project/Simpe3DProject/Assets/Scripts/MouseMovement.cs
+++ Project/Simpe3DProject/Assets/Scripts/MouseMovement.cs
@@ -7,12 +7,16 @@ public class MouseMovement : MonoBehaviour {
      public GameObject player;
      public float sensitivity;
  
-     void FixedUpdate ()
+     void FixedUpdate()
      {
-         Debug.Log(Input.GetAxis ("Mouse X"));
-         float rotateHorizontal = Input.GetAxis ("Mouse X");
-         float rotateVertical = Input.GetAxis ("Mouse Y");
-         transform.RotateAround (player.transform.position, -Vector3.up, rotateHorizontal * sensitivity); //use transform.Rotate(-transform.up * rotateHorizontal * sensitivity) instead if you dont want the camera to rotate around the player
-         transform.RotateAround (Vector3.zero, transform.right, rotateVertical * sensitivity); // again, use transform.Rotate(transform.right * rotateVertical * sensitivity) if you don't want the camera to rotate around the player
+         Debug.Log(Input.GetAxis("Mouse X"));
+         float rotateHorizontal = Input.GetAxis("Mouse X");
+         float rotateVertical = Input.GetAxis("Mouse Y");
+         // Use transform.Rotate(-transform.up * rotateHorizontal * sensitivity) instead
+         // if you dont want the camera to rotate around the player
+         transform.RotateAround(player.transform.position, -Vector3.up, rotateHorizontal * sensitivity);
+         // Again, use transform.Rotate(transform.right * rotateVertical * sensitivity)
+         // if you don't want the camera to rotate around the player
+         transform.RotateAround(Vector3.zero, transform.right, rotateVertical * sensitivity);
      }
 }
---
+++ Project/Simpe3DProject/Assets/Scripts/SetTextures.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * 
+ * @author Kaj Wortel
+ */
+public class SetTexture : MonoBehaviour {
+    /**----------------------------------------
+     * Constants.
+     * ----------------------------------------
+     */
+    Texture m_MainTexture, m_Normal, m_Metal;
+    private Renderer _Render;
+    
+
+	/**
+	 * Use this for initialization.
+	 */
+	void Start()
+	{
+		_Render = GetComponent<Renderer>();
+		/*
+        //Fetch the Renderer from the GameObject
+        m_Renderer = GetComponent<Renderer> ();
+
+        //Make sure to enable the Keywords
+        m_Renderer.material.EnableKeyword ("_NORMALMAP");
+        m_Renderer.material.EnableKeyword ("_METALLICGLOSSMAP");
+
+        //Set the Texture you assign in the Inspector as the main texture (Or Albedo)
+        m_Renderer.material.SetTexture("_MainTex", m_MainTexture);
+        //Set the Normal map using the Texture you assign in the Inspector
+        m_Renderer.material.SetTexture("_BumpMap", m_Normal);
+        //Set the Metallic Texture as a Texture you assign in the Inspector
+        m_Renderer.material.SetTexture ("_MetallicGlossMap", m_Metal);*/
+        Texture2D texture = Resources.Load<Texture2D>("Textures/potato");
+        setTexture(texture);
+    }
+    
+    public void setTexture(Texture tex)
+	{
+        //_Render.material.enableKeyword("");
+        _Render.material.SetTexture("MainTex", tex);
+    }
+	
+	/**
+	 *  Update is called once per frame.
+	 */
+	void Update()
+	{
+		
+	}
+	
+	
+}
