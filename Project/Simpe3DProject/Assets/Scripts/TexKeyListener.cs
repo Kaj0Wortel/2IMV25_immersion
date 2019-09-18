a599c19eb2e3c05bbfb518f2a8830911363dce06
Kaj0Wortel <kaj.wortel@gmail.com>
2019-09-18 15:56:47 +0200

    Added implementation + renamed file

---
+++ Project/Simpe3DProject/Assets/Scripts/TexKeyListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TexKeyListener : MonoBehaviour {
+
+	private boolean _SpaceDown = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown("space")) {
+			if (_SpaceDown) return;
+			_SpaceDown = true;
+
+			int max = 5; // TODO: get size of object
+			Random.Range(0, max);
+			// TODO: call texture randomization function.
+
+		} else {
+			_SpaceDown = false;
+		}
+	}
+
+
+}
